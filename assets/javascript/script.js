cab8f3ed4b53a2714ff6bfe8a37cfaf0b2e16bbd
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-21 09:01:39 -0300

    Initial commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,47 @@
+const novaTarefa = document.querySelector("#inserir-tarefa");
+const buttonAdicionar = document.querySelector("#btn-add");
+const buttonFeito = document.querySelectorAll("button.feito");
+const buttonRemover = document.querySelectorAll("button.remover");
+const atividadesCampo = document.querySelector(".atividades");
+const textoTarefaSpan = document.querySelectorAll(".tarefa span");
+
+
+buttonAdicionar.addEventListener("click", () => {
+    if (novaTarefa.value.trim() !== "") {
+        const div1 = document.createElement("div");
+        div1.setAttribute("class", "tarefa");
+        let c = new Atividade(novaTarefa.value);
+        const span = document.createElement("span");
+        span.innerHTML = c.tarefa;
+        console.log(c)
+        div1.appendChild(span);
+        const divButtons = document.createElement("div");
+        divButtons.setAttribute("class", "buttons")
+        div1.appendChild(divButtons);
+        const buttonDone = document.createElement("button");
+        buttonDone.setAttribute("class", "feito");
+        const icone1 = document.createElement("i");
+        icone1.className = "fa-solid fa-check";
+        buttonDone.appendChild(icone1);
+        divButtons.appendChild(buttonDone);
+        buttonDone.addEventListener("click", (event) => {
+            div1.classList.toggle("concluido");
+        });
+        const buttonRemove = document.createElement("button");
+        buttonRemove.setAttribute("class", "remover");
+        buttonRemove.innerHTML = "X";
+        buttonRemove.addEventListener("click", (event) => {
+            buttonRemove.parentNode.parentNode.remove()
+        });
+        divButtons.appendChild(buttonRemove);
+        atividadesCampo.appendChild(div1);
+        novaTarefa.value = "";
+        novaTarefa.placeholder = 'Digite a próxima tarefa aqui';
+    }
+})
+
+class Atividade {
+    constructor(tarefa) {
+        this.tarefa = tarefa
+    }
+}
